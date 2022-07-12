6e3857f94ddbfc98109c4b6c2062a50d4bdac13a
Justin Raišp <justin.raisp37@gmail.com>
2022-07-12 13:58:21 +0200

    Dodal premike, dovoljene so vse poteze

--- ChessMain.py
+++ ChessMain.py
@@ -24,10 +24,29 @@ def main():
     gs = ChessEngine.GameState()
     loadImages() #nardimo samo enkrat
     running = True
+    sqSelected = ()
+    playerClicks = []
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos() #(x,y) miske
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sqSelected == (row, col): #user clicked the same square twice
+                    sqSelected = () #unselect
+                    playerClicks = []
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected) #append for both 1st and 2nd clicks
+                if len(playerClicks) == 2: #after the second click
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected = ()
+                    playerClicks = []
+
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
@@ -41,7 +60,7 @@ def drawGameState(screen, gs):
 
 
 def drawBoard(screen): 
-    colors = [p.Color("white"), p.Color("brown")]
+    colors = [p.Color("white"), p.Color("light gray")]
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             color = colors[((r+c) % 2)]
